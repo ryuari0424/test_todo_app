ffccf9445ef0585b4ef95c639e7f380e32c05168
ryugo <ryu.ari.0424@gmail.com>
2023-08-19 19:10:22 +0900

    controller sakusei

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    public function index(){
+        $tasks = Task::all();
+        $user = Auth::user();
+
+        return view('task.index', ['tasks' => $tasks, 'user' => $user]);
+        
+    }
+
+    public function storeTask(Request $request){
+        
+    }
 }
